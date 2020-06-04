41d3ca0a4e9b2bea39a3be481d158b2466fccbda
alpersad <alpersad@uwaterloo.ca>
2020-06-04 15:59:51 -0400

    completed singed enchant

--- src/main/java/com/alpersad/loltomes/Singed/SingedEnchantment.java
+++ src/main/java/com/alpersad/loltomes/Singed/SingedEnchantment.java
@@ -2,13 +2,21 @@ package com.alpersad.loltomes.Singed;
 
 import com.alpersad.loltomes.Darkhax.EnchantmentTicking;
 import com.alpersad.loltomes.ModRegistry.ModEffects;
+import com.alpersad.loltomes.Vayne.SilverBoltOne;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import org.lwjgl.system.CallbackI;
+
+import java.util.Collection;
 
 public class SingedEnchantment extends EnchantmentTicking {
 
@@ -29,7 +37,18 @@ public class SingedEnchantment extends EnchantmentTicking {
     }
 
     @Override
-    public void onUserTick(LivingEntity user, int level) {
-        user.addPotionEffect(new EffectInstance(ModEffects.POISONTRAIL, 100));
+    public void onItemTick(LivingEntity user, int level, ItemStack item, EquipmentSlotType slot) {
+        if(!user.world.isRemote()) {
+            Collection<EffectInstance> potionlist = user.getActivePotionEffects();
+            for (EffectInstance effect : potionlist) {
+                if (effect.getEffectName().equals(PoisonTrail.name)) {
+                    item.damageItem(1, user, (p_220039_0_) -> {
+                        p_220039_0_.sendBreakAnimation(EquipmentSlotType.CHEST);
+                    });
+                    return;
+                }
+            }
+            user.addPotionEffect(new EffectInstance(ModEffects.POISONTRAIL, 50, 0, false, true));
+        }
     }
 }
