4d469c5514e8eb6bfc39223760cfcde18ead076f
alpersad <alpersad@uwaterloo.ca>
2020-06-04 12:21:41 -0400

    completed cloud generation effect

--- src/main/java/com/alpersad/loltomes/Singed/PoisonTrail.java
+++ src/main/java/com/alpersad/loltomes/Singed/PoisonTrail.java
@@ -1,15 +1,12 @@
 package com.alpersad.loltomes.Singed;
 
-import com.alpersad.loltomes.ModRegistry.ModEffects;
 import net.minecraft.entity.AreaEffectCloudEntity;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.EffectType;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.Direction;
-import net.minecraft.util.Rotation;
 import net.minecraft.util.math.Vec3d;
 
 public class PoisonTrail extends Effect {
@@ -22,10 +19,15 @@ public class PoisonTrail extends Effect {
     public void performEffect(LivingEntity entityLivingBaseIn, int amplifier) {
         super.performEffect(entityLivingBaseIn, amplifier);
 
-        AreaEffectCloudEntity areaeffectcloudentity = new AreaEffectCloudEntity(entityLivingBaseIn.getEntityWorld(), entityLivingBaseIn.getPositionVector().x, entityLivingBaseIn.getPositionVector().y, entityLivingBaseIn.getPositionVector().z);
+        Direction entityfacing = entityLivingBaseIn.getHorizontalFacing().getOpposite();
+        Vec3d cloudentitypos = new Vec3d(entityLivingBaseIn.getPositionVector().getX() + entityfacing.getXOffset(),
+                entityLivingBaseIn.getPositionVector().getY(),
+                entityLivingBaseIn.getPositionVec().getZ() + entityfacing.getZOffset());
+
+        AreaEffectCloudEntity areaeffectcloudentity = new AreaEffectCloudEntity(entityLivingBaseIn.getEntityWorld(), cloudentitypos.getX(), cloudentitypos.getY(), cloudentitypos.getZ());
         areaeffectcloudentity.setOwner(entityLivingBaseIn);
         areaeffectcloudentity.setColor(0x127c06);
-        areaeffectcloudentity.setDuration(100);
+        areaeffectcloudentity.setDuration(50);
         areaeffectcloudentity.setRadius(2.0f);
         areaeffectcloudentity.addEffect(new EffectInstance(Effects.POISON, 100));
         entityLivingBaseIn.getEntityWorld().addEntity(areaeffectcloudentity);
@@ -34,7 +36,7 @@ public class PoisonTrail extends Effect {
 
     @Override
     public boolean isReady(int duration, int amplifier) {
-        int k = 50 >> amplifier;
+        int k = 10 >> amplifier;
         if (k > 0) {
             return duration % k == 0;
         } else {
