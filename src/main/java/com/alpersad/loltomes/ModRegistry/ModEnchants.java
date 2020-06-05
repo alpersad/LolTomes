5dd5d86c1b1f453a889581f9e1f8f60445122d54
alpersad <alpersad@uwaterloo.ca>
2020-06-05 14:20:44 -0400

    added mundo enchant to registry

--- src/main/java/com/alpersad/loltomes/ModRegistry/ModEnchants.java
+++ src/main/java/com/alpersad/loltomes/ModRegistry/ModEnchants.java
@@ -2,6 +2,7 @@ package com.alpersad.loltomes.ModRegistry;
 
 import com.alpersad.loltomes.Ashe.AsheEnchantment;
 import com.alpersad.loltomes.LolTomes;
+import com.alpersad.loltomes.Mundo.MundoEnchantment;
 import com.alpersad.loltomes.Singed.SingedEnchantment;
 import com.alpersad.loltomes.Vayne.VayneEnchantment;
 import net.minecraft.enchantment.Enchantment;
@@ -16,5 +17,6 @@ public class ModEnchants {
     public static final RegistryObject<Enchantment> VAYNE = ENCHANTMENT.register("vayne", VayneEnchantment::new);
     public static final RegistryObject<Enchantment> SINGED = ENCHANTMENT.register("singed", SingedEnchantment::new);
     public static final RegistryObject<Enchantment> ASHE = ENCHANTMENT.register("ashe", AsheEnchantment::new);
+    public static final RegistryObject<Enchantment> MUNDO = ENCHANTMENT.register("mundo", MundoEnchantment::new);
 
 }
