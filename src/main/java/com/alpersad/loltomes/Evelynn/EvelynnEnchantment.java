3c14ccb7e01037d491f48b621ffa5976ce47386d
alpersad <alpersad@uwaterloo.ca>
2020-06-05 14:56:27 -0400

    created Evelynn enchantment

---
+++ src/main/java/com/alpersad/loltomes/Evelynn/EvelynnEnchantment.java
@@ -0,0 +1,31 @@
+package com.alpersad.loltomes.Evelynn;
+
+import com.alpersad.loltomes.Darkhax.EnchantmentTicking;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+
+public class EvelynnEnchantment extends EnchantmentTicking {
+
+    private static String name = "Evelynn";
+
+    public EvelynnEnchantment() {
+        super(Rarity.VERY_RARE, EnchantmentType.ARMOR_FEET, EquipmentSlotType.FEET);
+    }
+
+    @Override
+    public void onItemTick(LivingEntity user, int level, ItemStack item, EquipmentSlotType slot) {
+        if(!user.getEntityWorld().isRemote()){
+            user.addPotionEffect(new EffectInstance(Effects.INVISIBILITY, 50, 0, false, false));
+        }
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
