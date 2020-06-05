831db0793a0fd0797ccfe724a6d0d75d97f3eac1
alpersad <alpersad@uwaterloo.ca>
2020-06-05 14:56:49 -0400

    added evelynn enchantment to registry

--- src/main/java/com/alpersad/loltomes/ModRegistry/ModEnchants.java
+++ src/main/java/com/alpersad/loltomes/ModRegistry/ModEnchants.java
@@ -1,6 +1,7 @@
 package com.alpersad.loltomes.ModRegistry;
 
 import com.alpersad.loltomes.Ashe.AsheEnchantment;
+import com.alpersad.loltomes.Evelynn.EvelynnEnchantment;
 import com.alpersad.loltomes.LolTomes;
 import com.alpersad.loltomes.Mundo.MundoEnchantment;
 import com.alpersad.loltomes.Singed.SingedEnchantment;
@@ -18,5 +19,6 @@ public class ModEnchants {
     public static final RegistryObject<Enchantment> SINGED = ENCHANTMENT.register("singed", SingedEnchantment::new);
     public static final RegistryObject<Enchantment> ASHE = ENCHANTMENT.register("ashe", AsheEnchantment::new);
     public static final RegistryObject<Enchantment> MUNDO = ENCHANTMENT.register("mundo", MundoEnchantment::new);
+    public static final RegistryObject<Enchantment> EVELYNN = ENCHANTMENT.register("evelynn", EvelynnEnchantment::new);
 
 }
